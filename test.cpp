e41e19969c41e893006277385dd4998557bf0fde
75066808 <75066808@qq.com>
2022-02-17 18:49:55 -0800

    update uniform float sample & gaussian noise

--- test.cpp
+++ test.cpp
@@ -2,61 +2,78 @@
 using namespace emp;
 using namespace std;
 
-void test(int party, int number) 
+Float randomf01()
 {
-    Float a((float)number, ALICE);
-    Float b((float)number, BOB);
+	unsigned int seed = time(NULL);
 
-	Float res = a.ln();
-
-	cout << "Res: \t"<< res.reveal<double>() << endl;
-}
-
-// CDF: res = -b * sign(r - 0.5) * ln(1 - 2 * |r - 0.5|)
-void method1(int party, int seed, float b = 1.0)
-{
 	unsigned char key[16];
 	for (size_t i = 0; i < 16; i++)
 		key[i] = seed % 256;
 
-	PRG prg(key);
+	static PRG prg(key);
 
-	unsigned char ir;
-	prg.random_data(&ir, 1);
+	float fa, fb;
+	prg.random_data(&fa, 4);
+	prg.random_data(&fb, 4);
 
-	float fr = (float)ir / (1 << 8);
+	const unsigned int andmask = 0x3fffffff;
+	const unsigned int ormask  = 0x3f800000;
 
-	Float r(fr, ALICE);
+	Float fandmask(*(float*)&andmask);
+	Float formask(*(float*)&ormask);
+
+	Float aa(fa, ALICE);
+	Float bb(fb, BOB);
+
+	Float random = (aa ^ bb) & fandmask;
+	random = ((random & formask) ^ random ^ formask) - Float(1.0);
+
+	cout << "Random: \t"<< random.reveal<double>() << endl;
+
+	return random;
+}
+
+// CDF: res = -b * sign(r - 0.5) * ln(1 - 2 * |r - 0.5|)
+void laplacian1(float b = 1.0)
+{
+	Float r = randomf01();
 	Float t = r - Float(0.5);
-	
 	Float res = Float(-b) * (Float(1.0) - Float(2.0) * t.abs()).ln();
-	
 	res = res.If(t.less_equal(Float(0.0)), -res);
 
 	cout << "Res: \t"<< res.reveal<double>() << endl;
 }
 
 // Gamma: res = b * (ln(r1) - ln(r2))
-void method2(int party, int seed, int b = 1)
+void laplacian2(float b = 1.0)
 {
-	unsigned char key[16];
-	for (size_t i = 0; i < 16; i++)
-		key[i] = seed % 256;
+	Float r1 = randomf01();
+	Float r2 = randomf01();
+	Float res = Float(b) * (r1 / r2).ln();
 
-	PRG prg(key);
+	cout << "Res: \t"<< res.reveal<double>() << endl;
+}
 
-	unsigned char ia, ib;
-	prg.random_data(&ia, 1);
-	prg.random_data(&ib, 1);
+// Central Limit Theorem: res = X1 + ... + X12 - 6 (Xi in (0,1))
+void gaussian1()
+{
+	Float res = randomf01();
+	for (int i = 0; i < 11; i++)
+		res = res + randomf01();
 
-	float fa = (float)ia / (1 << 8);	
-	float fb = (float)ib / (1 << 8);
+	res = res - Float(6.0);
 
-	Float aa(fa, ALICE);
-	Float bb(fb, BOB);
+	cout << "Res: \t"<< res.reveal<double>() << endl;
+}
 
-	Float res = Float(b) * (aa / bb).ln();
+// Box–Muller method: res = sqrt(-2*lnU)cos(2*pi*V) (U, V in (0,1))
+void gaussian2()
+{
+	Float u = randomf01();
+	Float v = randomf01();
 
+	Float res = (Float(-2) * u.ln()).sqrt() * (Float(2 * 3.14) * v).cos();
+	
 	cout << "Res: \t"<< res.reveal<double>() << endl;
 }
 
@@ -72,7 +89,7 @@ int main(int argc, char** argv)
 	NetIO * io = new NetIO(party == ALICE ? nullptr : "127.0.0.1", port);
 
 	setup_semi_honest(io, party);
-	method1(party, time(NULL));
+	gaussian2();
 
 	cout << CircuitExecution::circ_exec->num_and()<<endl;
 	finalize_semi_honest();
